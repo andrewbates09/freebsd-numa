b8834debaa0ccbd3d2a1c727caeaf8dc9df43e89
Andrew Bates <andrewbates09@gmail.com>
2013-11-05 16:30:37 -0800

    Merging Josh's changes
    Merging josh's changes

--- freebsdnuma.h
+++ freebsdnuma.h
@@ -23,34 +23,43 @@
 
 
 /* ------- SYSCALL INTERFACE ------ */
-//Retrieves the memory affinity from the object specified by level, which and id
-// and returns it as a mask stored in the space provided by mask
-int cpuset_get_memory_affinity (cpulevel_t level,cpuwhich_t which,id_t id,size_t
-setsize, cpuset_t *mask);
-//Sets the memory affinity of the object specified by level,which and id to
-// the value stored in mask
-int cpuset_set_memory_affinity (cpulevel_t level,cpuwhich_t which,id_t id,size_t
-setsize, cpuset_t *mask);
+
+// Retrieves the memory affinity from the object specified by level, which and id and returns it as a mask stored in the space provided by mask
+int cpuset_get_memory_affinity (cpulevel_t level, cpuwhich_t which, id_t id, size_t setsize, cpuset_t *mask);
+
+// Sets the memory affinity of the object specified by level,which and id to the value stored in mask
+int cpuset_set_memory_affinity (cpulevel_t level, cpuwhich_t which, id_t id, size_t setsize, cpuset_t *mask);
+
 // Moves specified pages on specified nodes to new memory nodes.
-int move_pages ();
+long move_pages (int pid, unsigned long count, void **pages, const int *node int *status, int flags);
+
 // Attempts to move all pages of a process in specified nodes to specified new memory nodes.
-int migrate_pages ();
+int migrate_pages (int pid, unsigned long maxnodesm, const unsigned long *old_nodes, const unsigned long *new_nodes);
+
 
 /* ------- USERSPACE LIBRARY ------ */
 
 // If this returns true, the current machine is has NUMA architecture, and the following functions should be avaialable.
 // If this returns false, the current machine does not have NUMA architecture, and should not be allowed to use the following functions.
 int is_numa_available();
-//Returns an int representing the id of the CPU that the specified thread is
-//running on
+
+// Returns an int representing the id of the CPU that the specified thread is running on
 int get_current_cpu (int pid);
-//Gets the CPUs in the numa domain specified by the integer domain id.
+
+// Retrieves process
+struct proc get_current_proc (struct thread td);
+
+// Gets the CPUs in the numa domain specified by the integer domain id.
 struct pcpu get_cpus_in_domain (int domain);
-//Gets the current domain of the calling thread
+
+// Gets the current domain of the calling thread
 int get_current_domain (struct mem_affinity affin);
-//Sets the domain of the designated thread specified by pid to the specified numa domain
-int set_thread_on_domain (int pid, int domain);
+
+// Sets the domain of the designated thread specified by pid to the specified numa domain
+int set_thread_on_domain (struct thread td, int domain);
+
 // Migrates the memory pages that are NOT located on a thread's assinged NUMA node to its current assigned NUMA node.
 int migrate_estranged_pages ();
+
 
 #endif /* __FREE_BSD_NUMA_H__ */
