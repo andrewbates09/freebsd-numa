211da60ed87d543b5965d373dba4e826cf3215fd
Nicole Cooper <yanegomi@gmail.com>
2014-02-15 17:20:42 -0800

    style(9) fixes and move code to match upstream
    Also, make numanor into a "runnable" program (which
    really doesn't do much today, but still could be compiled
    and run in theory).

--- usr.bin/numanor/numanor.c
+++ usr.bin/numanor/numanor.c
@@ -13,6 +13,8 @@
 
 /* ----------- INCLUDES ----------- */
 
+#include <sys/types.h>
+
 #include <sys/freebsdnuma.h>        /* NUMA syscalls */
 
 
@@ -33,44 +35,49 @@
  *      use the following functions. The integer returned is the number of NUMA
  *      nodes available.
  */
-int is_numa_available(void)
+int
+is_numa_available(void)
 {
-    return 0;
+
+	return (0);
 }
 
 /* 
  * Function: set_thread_on_domain()
  * Input:
- *     int pid: The ID of thread to set on domain.
+ *     pid_t pid: The ID of thread to set on domain.
  *     int domain: The index of the target NUMA domain.
  * Output: Returns 1 on success. Returns 0 on failure.
  * Summary: Sets the domain of the designated thread specified by PID to the
  *      specified NUMA domain.
  */
-int set_thread_on_domain(int pid, int domain)
+int
+set_thread_on_domain(pid_t pid, int domain)
 {
-    return 0;
+
+	return (0);
 }
 
 /* 
  * Function: set_memory_policy()
  * Input:
- *     int pid: The ID of thread to set the memory policy for.
+ *     pid_t pid: The ID of thread to set the memory policy for.
  *     int thread_policy: Specifies the thread’s policy, defined in
  *          freebsdnuma.h (NUMA_POLICY_NEAREST, NUMA_POLICY_INTERLEAVE)
  * Output: Returns 1 on success. Returns 0 on failure.
  * Summary: Set specific memory policy to thread (default action: nearest first
  *      touch)
  */
-int set_memory_policy(int pid, int thread_policy)
+int set_memory_policy(pid_t pid, int thread_policy)
 {
-    return 0;
+
+	return (0);
 }
 
 /* 
  * Function: move_thread()
  * Input: 
- *     int pid: The ID of thread to move.
+ *     pid_t pid: The ID of thread to move.
  *     int domain: The ID of NUMA domain to move to.
  *     int mem_flag: Specify the memory action for moving a thread (MEM_LEAVE
  *          MEM_MIGRATE).
@@ -79,7 +86,16 @@ int set_memory_policy(int pid, int thread_policy)
  *      preferences to the specified NUMA domain. Memory pages are either left 
  *      estranged or migrated, depending on policy defined by mem_flag.
  */
-int move_thread(int pid, int domain, int mem_flag)
+int
+move_thread(pid_t pid, int domain, int mem_flag)
 {
-    return 0;
+
+	return (0);
+}
+
+int
+main(void)
+{
+
+	return (0);
 }
