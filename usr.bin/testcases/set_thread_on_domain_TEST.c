f9915608e4527c749155fd7b0ba5fa9721f6da25
jackoblades <dustin.walker@email.wsu.edu>
2014-02-17 15:33:32 -0800

    Tests are aware of is_numa_available() + more
    Added new test: tstod_no_numa()
    Added preconditions comments
    Added is_numa_available() calls to all tests

--- usr.bin/testcases/set_thread_on_domain_TEST.c
+++ usr.bin/testcases/set_thread_on_domain_TEST.c
@@ -38,6 +38,7 @@ void test_set_thread_on_domain (void);
 void tstod_run_test(int result, char* testname);
 
 int tstod_valid_input (void);
+int tstod_no_numa (void);
 int tstod_negative_pid (void);
 int tstod_zero_pid (void);
 int tstod_init_pid (void);
@@ -50,12 +51,13 @@ int tstod_outofrange_domain (void);
 void test_set_thread_on_domain (void)
 {
     tstod_run_test( tstod_valid_input(), "tstod_valid_input" );
+    tstod_run_test( tstod_no_numa(), "tstod_no_numa" );
     tstod_run_test( tstod_negative_pid(), "tstod_negative_pid" );
     tstod_run_test( tstod_zero_pid(), "tstod_zero_pid" );
     tstod_run_test( tstod_init_pid(), "tstod_init_pid" );
-    tstod_run_test( tstod_init_pid(), "tstod_outofrange_pid" );
+    tstod_run_test( tstod_outofrange_pid(), "tstod_outofrange_pid" );
     tstod_run_test( tstod_negative_domain(), "tstod_negative_domain" );
-    tstod_run_test( tstod_negative_domain(), "tstod_outofrange_domain" );
+    tstod_run_test( tstod_outofrange_domain(), "tstod_outofrange_domain" );
 
     return;
 }
@@ -75,6 +77,7 @@ void tstod_run_test (int result, char* testname)
 
 /*
 * Function Name: tstod_valid_input
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = 2
 *                   domain = 1
 * Expected Result: 1 (success)
@@ -82,6 +85,8 @@ void tstod_run_test (int result, char* testname)
  // I am trying to make this set of test cases more specifically broken up.
 int tstod_valid_input (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(2, 1) == SUCCESS)
     {
         return SUCCESS;
@@ -93,13 +98,39 @@ int tstod_valid_input (void)
 }
 
 /*
+* Function Name: tstod_no_numa
+* Pre-Conditions: numa_available = 0 (false)
+* Input Parameters: pid    = 2
+*                   domain = 1
+* Expected Result: 0 (failure)
+*/
+ // This test case will need to manually change a global
+ // numa_available flag in order to trick the system
+int tstod_no_numa (void)
+{
+    is_numa_available(); // Returns as false.
+
+    if (set_thread_on_domain(2, 1) == FAILURE)
+    {
+        return SUCCESS;
+    }
+    else
+    {
+        return FAILURE;
+    }
+}
+
+/*
 * Function Name: tstod_negative_pid
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = -1
 *                   domain = 1
 * Expected Result: 0 (failure)
 */
 int tstod_negative_pid (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(-1, 1) == FAILURE)
     {
         return SUCCESS;
@@ -112,12 +143,15 @@ int tstod_negative_pid (void)
 
 /*
 * Function Name: tstod_zero_pid
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = 0 (kernel process)
 *                   domain = 1
 * Expected Result: 0 (failure)
 */
 int tstod_zero_pid (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(0, 1) == FAILURE)
     {
         return SUCCESS;
@@ -130,12 +164,15 @@ int tstod_zero_pid (void)
 
 /*
 * Function Name: tstod_init_pid
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = 1 (init process)
 *                   domain = 1
 * Expected Result: 0 (failure)
 */
 int tstod_init_pid (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(1, 1) == FAILURE)
     {
         return SUCCESS;
@@ -148,12 +185,15 @@ int tstod_init_pid (void)
 
 /*
 * Function Name: tstod_outofrange_pid
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = 1000000000 (hopefully out of range)
 *                   domain = 1
 * Expected Result: 0 (failure)
 */
 int tstod_outofrange_pid (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(1000000000, 1) == FAILURE)
     {
         return SUCCESS;
@@ -166,12 +206,15 @@ int tstod_outofrange_pid (void)
 
 /*
 * Function Name: tstod_negative_domain
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = 2
 *                   domain = -1
 * Expected Result: 0 (failure)
 */
 int tstod_negative_domain (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(2, -1) == FAILURE)
     {
         return SUCCESS;
@@ -184,12 +227,15 @@ int tstod_negative_domain (void)
 
 /*
 * Function Name: tstod_outofrange_domain
+* Pre-Conditions: numa_available = 1 (true)
 * Input Parameters: pid    = 2
 *                   domain = 1000 (hopefully out of range)
 * Expected Result: 0 (failure)
 */
 int tstod_outofrange_domain (void)
 {
+    is_numa_available(); // Returns as true.
+
     if (set_thread_on_domain(2, 1000) == FAILURE)
     {
         return SUCCESS;
