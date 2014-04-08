48b750c6b08e167a5cbfb633367b47dd90e08a79
Garrett Cooper <garrett.cooper@zonarsystems.com>
2014-04-08 15:15:14 -0700

    Clean up trailing whitespace

--- usr.bin/numanor/numanor.h
+++ usr.bin/numanor/numanor.h
@@ -1,5 +1,5 @@
 /*-
- * Copyright (c) 2014 EMC Corporation 
+ * Copyright (c) 2014 EMC Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,13 +28,13 @@
 
 /*
  * FreeBSD NUMA project - userspace
- * 
+ *
  * The numanor.h userspace library takes advantage of NUMA functionality exposed
  * by the freebsdnuma.h syscalls.  In short, this abstracts the syscalls
  * included in freebsdnuma.h and makes them more user friendly.  Users should be
  * able to make their application NUMA-aware by using these functions.  Also
  * included are basic functions to test NUMA.
- * 
+ *
  * Last Edited: December 08, 2013
  */
 
@@ -49,7 +49,7 @@
 
 /* ---------- DEFINITIONS --------- */
 
-/* 
+/*
  * numa_count: The number of distinct NUMA nodes on the system.
  * numa_cpus: An array of cpuset structs that denotes the number of distinct
  *      NUMA CPUs. See get_numa_cpus() in freebsdnuma.h.
@@ -74,21 +74,21 @@ extern uint16_t *numa_weights;
 
 /* ---------- USERSPACE LIBRARY --- */
 
-/* 
+/*
  * Function: is_numa_availiable()
  * Input: void
  * Output: Returns 0 if NUMA is not available, otherwise returns the number of
  *      NUMA nodes as integer.
  * Summary: If this function evaluates as true (1-n nodes), the current machine
  *      is found to have NUMA architecture, and the functions included below
- *      should be available.  If this evaluates to false (0), the current 
+ *      should be available.  If this evaluates to false (0), the current
  *      machine does not have NUMA architecture, and should not be allowed to
  *      use the following functions. The integer returned is the number of NUMA
  *      nodes available.
  */
 int is_numa_available(void);
 
-/* 
+/*
  * Function: set_thread_on_domain()
  * Input:
  *     int pid: The ID of thread to set on domain.
@@ -100,7 +100,7 @@ int is_numa_available(void);
 int set_thread_on_domain(int pid,
                          int domain);
 
-/* 
+/*
  * Function: set_memory_policy()
  * Input:
  *     int pid: The ID of thread to set the memory policy for.
@@ -113,16 +113,16 @@ int set_thread_on_domain(int pid,
 int set_memory_policy(int pid,
                       int thread_policy);
 
-/* 
+/*
  * Function: move_thread()
- * Input: 
+ * Input:
  *     int pid: The ID of thread to move.
  *     int domain: The ID of NUMA domain to move to.
  *     int mem_flag: Specify the memory action for moving a thread (MEM_LEAVE
  *          MEM_MIGRATE).
  * Output: Returns 1 on success. Returns 0 on failure.
  * Summary: Moves a thread to a specified NUMA node and sets memory allocation
- *      preferences to the specified NUMA domain. Memory pages are either left 
+ *      preferences to the specified NUMA domain. Memory pages are either left
  *      estranged or migrated, depending on policy defined by mem_flag.
  */
 int move_thread(int pid,
